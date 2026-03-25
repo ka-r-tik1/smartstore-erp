3f4a2b3914b045e1dc43c826c8e59d3ecdec0dae
ka-r-tik1 <vairatbkartik@gmail.com>
2026-03-25 10:58:15 +0530

    fix: start.sh - sirf uvicorn chalao, pip build phase mein hoga

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 cd "Backend files"
-pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+python3 -m uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
